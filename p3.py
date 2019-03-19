9171555601853fe1bb3421e782c31829ad7853db
manasa1463 <48721818+manasa1463@users.noreply.github.com>
2019-03-19 20:17:43 +0530

    Add files via upload

---
+++ p3.py
@@ -0,0 +1,12 @@
+list1=['a','e','i','o','u','A','E','I','O','U']
+str="b c d f g h j k l m n  p q r s t v w x y z B C D F G H J K L M N P Q R S T V W X Y Z"
+a=input()
+if a in list1:
+    print("vowel")
+elif a in str:
+    print("Consonant")
+else:
+    print("invalid")    
+    
+
+    
