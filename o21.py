1200f56c0135edee0e3c93bc0a3f26d7f5114caf
manasa1463 <48721818+manasa1463@users.noreply.github.com>
2019-03-22 22:31:09 +0530

    Update o21.py

--- o21.py
+++ o21.py
@@ -2,6 +2,5 @@ n,a,d=input().split(' ')
 n1=int(n)
 a1=int(a)
 d1=int(d)
-if (1<=n1<=100000)and(1<=a1<=100000)and(1<=d1<=100000):
-    k=((n1/2)*(2*a1+(n1-1)*d1))
-    print(int(k))
+k=((n1/2)*(2*a1+(n1-1)*d1))
+print(int(k))
