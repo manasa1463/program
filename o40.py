50bef0f52cdf111b2e9ccfe0536bda8e8164a4e4
manasa1463 <48721818+manasa1463@users.noreply.github.com>
2019-03-24 00:55:51 +0530

    Add files via upload

---
+++ o40.py
@@ -0,0 +1,17 @@
+n=input()
+n=int(n)
+n1=1
+n2=1
+if(n==1):
+    print(n1,end=' ')
+count=0
+if(n>1):
+    while(count<n):
+        if(count==n-1):
+            print(n1,end='')
+        else:
+            print(n1,end=' ')
+        nth=n1+n2
+        n1=n2
+        n2=nth
+        count=count+1
