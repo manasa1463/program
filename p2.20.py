6f670cdb513d2b2a94bdd2dc6ef048e5dc7343a6
manasa1463 <48721818+manasa1463@users.noreply.github.com>
2019-04-11 16:39:54 +0530

    Update p2.20.py

--- p2.20.py
+++ p2.20.py
@@ -1,3 +1,10 @@
 s=input()
 for i in range(len(s)):
-    print(chr(ord(s[i])+3),end='')
+    if (((ord('z')-ord(s[i]))>=3) and ((ord('z')-ord(s[i]))<=26)) or (((ord('Z')-ord(s[i]))>=3) and ((ord('Z')-ord(s[i]))>=3)):
+        print(chr(ord(s[i])+3),end='')
+    elif (ord('z')-ord(s[i]))<3:
+        k=(ord(s[i])-ord('z'))+97+2
+        print(chr(k),end='')
+    else:
+        k=(ord(s[i])-ord('Z'))+65+2
+        print(chr(k),end='')
