5db70ae7413ee9c50b99a8bebfe3bde404bb14bb
manasa1463 <48721818+manasa1463@users.noreply.github.com>
2019-04-11 13:04:14 +0530

    Update p2.12.py

--- p2.12.py
+++ p2.12.py
@@ -1,14 +1,12 @@
 n,k=map(int,input().split())
 l1=[]
 l=[int(i) for i in input().split()]
-try:
-    for i in l[-k:]:
-        l1.append(i)
-    for i in l[:-k]:
-        l1.append(i)
-except IndexError:
-    for i in range((len(l)-1)):
-        l1.append(i)
+if k>n:
+    k=k%n
+for i in l[-k:]:
+    l1.append(i)
+for i in l[:-k]:
+    l1.append(i)
 for i in range((len(l1)-1)):
     print(l1[i],end=' ')
 print(l1[(len(l1)-1)],end='')    
