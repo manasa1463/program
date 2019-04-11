8bd09e401ee3de93c79bb58656410fe8818c1fcb
manasa1463 <48721818+manasa1463@users.noreply.github.com>
2019-04-11 12:04:56 +0530

    Update p2.12.py

--- p2.12.py
+++ p2.12.py
@@ -1,12 +1,14 @@
 n,k=map(int,input().split())
+l1=[]
 l=[int(i) for i in input().split()]
 try:
     for i in l[-k:]:
-        print(i,end=' ')
-        for i in l[:-k-1]:
-            print(i,end=' ')
-            print(l[-k-1],end='')    
+        l1.append(i)
+        for i in l[:-k]:
+            l1.append(i)
 except IndexError:
     for i in range((len(l)-1)):
-        print(l[i],end=' ')
-    print(l[(len(l)-1)],end='')   
+        l1.append(i)
+for i in range((len(l1)-1)):
+       print(l1[i],end=' ')
+print(l1[(len(l1)-1)],end='')       
