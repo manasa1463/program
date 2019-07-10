e8b207f1b54f712fdba9f38294f72dd08fea473e
manasa1463 <48721818+manasa1463@users.noreply.github.com>
2019-07-10 21:05:31 +0530

    Add files via upload

---
+++ p2.48.py
@@ -0,0 +1,8 @@
+n=int(input())
+l=[]
+for i in range(1,n+1):
+    if n%i==0 and i%2!=0:
+        l.append(i)      
+for i in range(len(l)-1):
+    print(l[i],end=' ')
+print(l[len(l)-1],end='')        
