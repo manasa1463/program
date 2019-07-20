86dee37b38f6c039c01be344b8ad882b8a5f30bf
manasa1463 <48721818+manasa1463@users.noreply.github.com>
2019-07-20 10:42:38 +0530

    Add files via upload

---
+++ p2.62.py
@@ -0,0 +1,8 @@
+import math
+n=int(input())
+for i in range(1,n):
+    k=n/i
+    if k%2!=0:
+        if k is not 0 and math.floor((k%2))==math.ceil((k%2)):
+            print(i)
+            break;
