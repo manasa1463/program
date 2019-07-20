3f9f53a02dbe392b6715b1dddad07bebef0a16ca
manasa1463 <48721818+manasa1463@users.noreply.github.com>
2019-07-20 10:05:01 +0530

    Update p2.59.py

--- p2.59.py
+++ p2.59.py
@@ -1,15 +1,13 @@
 import re
 n=int(input())
-k=list(map(str,input().split()))
-st=''.join(k)
+st=input()
 count=0
 for m in re.finditer('0',st):
     count=m.start() 
 l=[]
 for i in range(count):
-    if k[i]=='1':
-        l.append(int(k[i]))
-print(l)
+    if st[i]=='1':
+        l.append(int(st[i]))
 for i in range(len(l)-1):
     print(l[i],end=' ')
 print(l[len(l)-1],end='')  
