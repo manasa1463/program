87cc87ad59d55c714a9a54af36d2bbf3c2f5fe88
manasa1463 <48721818+manasa1463@users.noreply.github.com>
2019-07-20 10:29:32 +0530

    Update p2.61.py

--- p2.61.py
+++ p2.61.py
@@ -1,6 +1,7 @@
 n,k=map(int,input().split())
-l=sum(list(map(int,input().split())))
-if l==k:
+l=list(map(int,input().split()))
+l1=[ (i,j) for i in range(len(l)) for j in range(i+1,len(l)) if l[i]+l[j]==k]
+if len(l1)>0:
     print("yes")
 else:
     print("no")    
